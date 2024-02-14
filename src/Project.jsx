e0da209b8ced20820d336619bcaca2471a5c425b
Nyeem Davis <nyeemdavis4@gmail.com>
2024-02-14 03:32:08 -0500

    Returns a componenet to hold project

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,10 +1,26 @@
+import React from 'react'
+import ProjectForm from './ProjectForm';
+import ProjectsContainer from './ProjectsContainer';
 
+import {useState} from 'react';
 
-export default function Project () {
+export default function Project ({ project }) {
+    // Allow user to edit information. Stored in state
+    
 
     return (
         <>
+            <div className='project'>
+                <h2>Name: {project.projectName}</h2>
+                <p>Description: {project.description}</p>
+                <p>Deadline: {project.deadline}</p>
+                <br></br>
+                <h3 className='tasks-anchor'>To-Do</h3>
+            </div>
+
+            
         </>
     );
 };
+
 
