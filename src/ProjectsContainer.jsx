253a70e5f9032d5a900eb77d90ed648a69bf8a2e
Nyeem Davis <nyeemdavis4@gmail.com>
2024-02-14 03:33:18 -0500

    Proprely renders project on submit. Added useState. Methods to add and delete project. Loop through project state and render

--- src/ProjectsContainer.jsx
+++ src/ProjectsContainer.jsx
@@ -1,14 +1,34 @@
 import { useState } from "react";
+import Project from "./Project";
+import ProjectForm from "./ProjectForm";
 
-export default function ProjectsContainer () {
-    // Use State to updaet the projects in the list
+
+function ProjectsContainer () {
+    // Use State to update the projects in the list
     const [projects, setProjects] = useState([]);
 
+    const addProject = (project) => {
+        setProjects([...projects, project])
+    };
+
+    // Deleting a project
+    const deleteProject = (project) => {
+        const updatedProjects = projects.filter((p) => p !== project);
+        setProjects(updatedProjects);
+      };
 
     return (
         <div className="pojects-container">
-
+            {/* Hook add project method to form with render */}
+            <ProjectForm addProject={addProject} projects={projects} />
+            <h2>Projects: {projects.length}</h2>
+            {/* Loop through and render each project */}
+            {projects.map((project, index) => (
+        <Project key={index} project={project} />
+      ))}
         </div>
     );
-}
+};
+
+export default ProjectsContainer;
 
