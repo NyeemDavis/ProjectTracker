aeb74a966c84164568c35c36a4a04bd8bc2ed84d
Nyeem Davis <nyeemdavis4@gmail.com>
2024-02-14 03:31:46 -0500

    Added buttons to add and delete projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Button from './Button';
 import ProjectForm from './ProjectForm';
 import './App.css'
+import ProjectsContainer from './ProjectsContainer';
 
 function App() {
  const handleClick = () => {
-
+  console.log('Clicked')
  };
 
 
@@ -12,10 +13,11 @@ function App() {
   return (
     <div>
       <h1 className='title'>Project Progress Tracker</h1>
-      {/* Button to add Project, Pulls up form */}
-      <Button onClick={handleClick} text={'Add Project'}></Button>
-
-      <ProjectForm></ProjectForm>
+      {/* Button to add Project, Pulls up form. Another to Delete */}
+      <Button onClick={handleClick} text={'Add Project'} />
+      <Button onClick={handleClick} text={"Delete Project"} />
+      
+      <ProjectsContainer />
     </div>
   );
 };
